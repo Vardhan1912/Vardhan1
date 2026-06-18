59eab6bfef4767d7e37dd55b5317e044edac4369
Vardhan1912 <ddokalavardhansahit@gmail.com>
2026-06-18 11:56:46 +0530

    Add files via upload

---
+++ Surya.java
@@ -0,0 +1,30 @@
+class Surya
+{
+
+ void main()
+
+{
+
+System.out.println("hello world"); 
+
+hi();
+welcome();
+hello();
+}
+void hi()
+
+{
+	
+System.out.println("peram");
+System.out.println("Vardhan");
+}
+void welcome()
+{
+	System.out.println("welcome to java world");
+}
+void hello()
+{
+	System.out.println("Lokesh");
+	
+	}
+}
